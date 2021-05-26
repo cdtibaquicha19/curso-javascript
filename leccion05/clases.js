aa6c71cef882ba86a771a35bf916eceaf7bc95b1
Cristyan Tibaquicha <cdtibaquicha19@gmail.com>
2021-05-25 21:52:04 -0500

    ejercicio creación de objetos y clases en js

--- leccion05/clases.js
+++ leccion05/clases.js
@@ -1,7 +1,22 @@
 class Persona{
+
+    static contadorPersonas = 0 ;
+
+    static get MAX_OBJ(){
+        return 3 ;
+    }
+
     constructor(nombre , apellido ){
+
         this._nombre = nombre ;
         this._apellido = apellido;
+        if(Persona.contadorPersonas < Persona.MAX_OBJ){
+            this.idPersona = ++Persona.contadorPersonas ;
+        }else{
+            console.log('se han superado el maximo de objetos persona permitidos');
+        }
+    
+
     }
     get nombre(){
         return this._nombre ;
@@ -18,17 +33,27 @@ class Persona{
     }
     nombreCompleto () 
     {
-        return this.nombre +' '+ this.apellido;     
+        return this.idPersona +' '+  this.nombre +' '+ this.apellido;     
+    }
+    toString(){
+        return this.nombreCompleto();
+    }
+    static saludar(){
+
+        console.log("saludos desde metodo static");
+
+    }
+    static saludar2(persona){
+        console.log("saludos "+persona.nombre +" desde metodo static")
     }
 
 }
+
 
 class Empleado  extends Persona{
     constructor(nombre , apellido ,departamento){
         super(nombre,apellido);
         this._departamento = departamento;
-
-       
     }
     get departamento(){
         return this._departamento;
@@ -36,21 +61,26 @@ class Empleado  extends Persona{
     set departamento(departamento){
         this._departamento = departamento ; 
     }
+
+    //sobrescritura de una funcion 
+    nombreCompleto(){
+        return super.nombreCompleto()+ ' ' + this._departamento;
+    }
+    
 }
 
-
-let empleado1 = new Empleado ('cristyan','tibaquicha', 'sistemas');
-console.log(empleado1);
-
 let persona1 = new Persona('Cristyan','Tibaquicha');
-
-console.log(persona1.nombre)
-console.log(persona1.nombreCompleto())
-console.log(persona1);
+let persona2 = new Persona('Cristyan','Tibaquicha');
+let empleado1 = new Persona('Dary','Rueda','Administracion');
 
 
-let persona2 = new Persona('yurany','rueda');
-console.log(persona2);
-console.log(persona2.nombreCompleto());
+
+console.log(persona2.toString());
+console.log(Persona.MAX_OBJ);
+
+
+console.log(Persona.contadorPersonas);
+
+
 
 
