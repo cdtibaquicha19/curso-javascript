fdf9982bd0c2896e728776ba2c56a11c033038df
Cristyan david tibaquicha larrota <cdtibaquicha19@gmail.com>
2022-09-13 15:46:32 -0500

    Carga desde pc 2 modulo lección 09

--- Leccion09/09-01-Clases.js
+++ Leccion09/09-01-Clases.js
@@ -1,31 +1,66 @@
 class Persona {
-    constructor( nombre, apellido ,documento){
-        this._nombre = nombre;
-        this._apellido = apellido;  
-        this._documento = documento;
 
-    }
+    static contadorObjetosPersona = 0 ;
+
+    constructor(nombre, apellido){
+        this._nombre = nombre ;
+        this._apellido  = apellido ; 
+        Persona.contadorObjetosPersona++;
+        }
     get nombre(){
         return this._nombre;
     }
     set nombre(nombre){
-        this._nombre = nombre ;
-    }
+        this._nombre=nombre;
+    }  
     get apellido(){
         return this._apellido;
-    }
-    set apellido(apellido){
-        this._apellido = apellido ;
-    }
-
     
+    }
+    set apellido (apellido){
+        this._apellido = apellido ; 
+    } 
+    nombreCompleto(){
+        return this.nombre +" "+ this.apellido ; 
+    }
+    toString(){
+        return this.nombreCompleto() ;
+    }
+    static saludar (){
+        console.log("saludos desde metodo static ")
+    }
+    static saludar2 (persona){
+        console.log("Saludos " + persona.nombreCompleto())
+    }
+    
+}
+class Empleado extends Persona{
+    constructor(nombre, apellido, departamento){
+        super(nombre,apellido);// llamar al constructor de la clase padre 
+        this._departamento = departamento;
+    }
+    get departamento (){
+        return this._departamento;
+    }
+    set departamento (departamento){
+        this._departamento = departamento;
+    }
+    //sobrescritura 
+    nombreCompleto(){
+        return super.nombreCompleto() + ", Departamento :"+this._departamento
+    }
 }
 
 
-let persona1 = new Persona( "Cristyan" , "Tibaquicha"  );
-persona1.nombre = "David";
-persona1.apellido= "larrota"
+let persona1 = new Persona ('juan',' perez');
 
+console.log( persona1 );
 
-console.log( JSON.stringify(persona1 ) );
+let empleado1 = new Empleado('cristyan' , ' tibaquicha ', 'sistemas');
+console.log(empleado1);
+console.log(empleado1.nombreCompleto());
 
+console.log(empleado1.toString());
+
+Persona.saludar2(persona1);
+console.log (Persona.contadorObjetosPersona);
